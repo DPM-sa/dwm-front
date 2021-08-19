4d7d7649f2c506694019d0eb03c87cc27afe0dc0
facudelucia <facundodeluciajudo@gmail.com>
2021-08-19 11:52:32 -0300

    add github actions

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/dwm-front/' : ''
+}
