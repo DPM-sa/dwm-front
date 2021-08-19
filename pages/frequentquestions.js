d9b239a56bfccd446d42cf6138cdb950d4ccf508
facudelucia <facundodeluciajudo@gmail.com>
2021-08-19 12:10:15 -0300

    cambios

--- pages/frequentquestions.js
+++ pages/frequentquestions.js
@@ -6,7 +6,7 @@ import { useStateValue } from '../context/StateProvider';
 
 const FrequentQuestions = () => {
     const [{ }, dispatch] = useStateValue()
-    const handleOpen = (e, i) => {
+    const handleOpen = (i) => {
         var coll = document.getElementsByClassName("frequent-question-button");
         var element = coll[i].nextSibling
         if (element.style.display === 'none') {
@@ -31,35 +31,35 @@ const FrequentQuestions = () => {
                 <div>
                     <ul className="frequent-questions-list">
                         <li>
-                            <div onClick={(e) => handleOpen(e, 0)} className="frequent-question-button">
+                            <div onClick={(e) => handleOpen(0)} className="frequent-question-button">
                                 <p>¿Las máquinas publicadas son argentinas?</p>
                                 <FontAwesomeIcon icon={faChevronDown} />
                             </div>
                             <p className="frequent-question-content">No, las importamos desde China en general ya que su tecnologia es la más avanzada relacionada con la fabricación y reciclaje de productos de plástico, polietileno, film, caucho, papel, cartón y metal.</p>
                         </li>
                         <li>
-                            <div onClick={(e) => handleOpen(e, 1)} className="frequent-question-button">
+                            <div onClick={(e) => handleOpen(1)} className="frequent-question-button">
                                 <p>¿La entrega es inmediata?</p>
                                 <FontAwesomeIcon icon={faChevronDown} />
                             </div>
                             <p className="frequent-question-content">Hay operaciones que son a pedido, así encontramos las mejores alternativas para el funcionamiento deseado y otras están disponibles para entrega inmediata.</p>
                         </li>
                         <li>
-                            <div onClick={(e) => handleOpen(e, 2)} className="frequent-question-button">
+                            <div onClick={(e) => handleOpen(2)} className="frequent-question-button">
                                 <p>¿Con qué financiación cuentan?</p>
                                 <FontAwesomeIcon icon={faChevronDown} />
                             </div>
                             <p className="frequent-question-content">Todo dependera del tipo de máquina que sea solicitada y del cliente. Actualmente tenemos excelentes planes de pagos para nuestros clientes más fieles.</p>
                         </li>
                         <li>
-                            <div onClick={(e) => handleOpen(e, 3)} className="frequent-question-button">
+                            <div onClick={(e) => handleOpen(3)} className="frequent-question-button">
                                 <p>¿Se pueden solicitar desde cualquier parte del país?¿Realizan envíos?</p>
                                 <FontAwesomeIcon icon={faChevronDown} />
                             </div>
                             <p className="frequent-question-content">Si, podes comprar desde cualquier provincia y nos encargamos de realizar la entrega ya que contamos con la logística necesaria para que recibas en tiempo y forma el producto.</p>
                         </li>
                         <li>
-                            <div onClick={(e) => handleOpen(e, 4)} className="frequent-question-button">
+                            <div onClick={(e) => handleOpen(4)} className="frequent-question-button">
                                 <p>¿En caso de necesitar asesoría o mas informacion para encontrar el producto que se ajuste a mis necesidades, cómo me contacto?</p>
                                 <FontAwesomeIcon icon={faChevronDown} />
                             </div>
